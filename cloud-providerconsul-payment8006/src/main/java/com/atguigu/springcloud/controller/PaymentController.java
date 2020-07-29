ffedef139ddb51b4d5f0efae0281e4f8acf95a9f
xuhao <1017504059@qq.com>
2020-07-29 16:45:31 +0800

    consul的配置与测试

---
+++ cloud-providerconsul-payment8006/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @auther xuhao
+ * @create 2020年7月29日16:21:04
+ */
+@RestController
+@Slf4j
+public class PaymentController
+{
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/consul")
+    public String paymentConsul()
+    {
+        return "springcloud with consul: "+serverPort+"\t   "+ UUID.randomUUID().toString();
+    }
+}
--- xuhao.md
+++ xuhao.md
@@ -3,5 +3,28 @@
    consul是一套开源的分布式服务发现和配置管理系统，由HashiCrop公司用go语言编写。
    提供了微服务系统中心的服务治理、配置中心、控制总线等功能。这些功能中的每一个都可以根绝需求单独使用，
 也可以一起使用以构建全方位的服务网格。
-
- K
+   ~~~~
+ 下载Consul
+ 
+ 官网下载地址： https://www.consul.io/downloads.html
+ 
+ 官网下载太慢，我找了一个百度云地址，提取码：247m
+ https://pan.baidu.com/s/1nMXRms3_ZPhgqawrcEcsdA
+ 
+ 安装Consul
+ 
+ 先附上官网的 Consul 安装动画片 o(￣︶￣)o ：
+ https://learn.hashicorp.com/consul/getting-started/services
+ 
+ 步骤：
+ 
+ 下载完成后解压，根据自己实际情况选择路径
+ 
+ 解压完成后，在解压路径下的地址栏输入“cmd”，打开命令行窗口。并键入“consul”，若出现一连串英文则表示安装成功，
+ 启动：
+ 
+ 命令consul agent -dev启动，看到Consul agent running! 启动成功
+ 
+ 通过以下地址可以访问 Consul 首页
+ http://localhost:8500
+ 
