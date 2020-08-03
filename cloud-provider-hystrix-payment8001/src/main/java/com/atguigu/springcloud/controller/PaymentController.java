a0b42483b704ae60d9766023dc09e7f263ae1640
xuhao <1017504059@qq.com>
2020-08-03 13:18:50 +0800

    服务熔断的流程

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -30,4 +30,12 @@ public class PaymentController {
         log.info("***** timeOut " +timeOut);
         return timeOut;
     }
+    //====服务熔断
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id)
+    {
+        String result = paymentService.paymentCircuitBreaker(id);
+        log.info("****result: "+result);
+        return result;
+    }
 }
