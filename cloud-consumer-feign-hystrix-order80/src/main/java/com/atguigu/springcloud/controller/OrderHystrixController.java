56fc64803d63bbcfcb057483be2f8e1483475949
xuhao <1017504059@qq.com>
2020-08-03 10:04:45 +0800

    80调用对自己进行的降级保护

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,8 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,17 +16,39 @@ public class OrderHystrixController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
 
+    /**
+     * 成功操作
+     * @param id
+     * @return
+     */
     @GetMapping("/consumer/payment/hystrix/ok/{id}")
     public String paymentInfo_ok(@PathVariable("id") Integer id) {
         String info_ok = paymentHystrixService.paymentInfo_ok(id);
-        log.info("*******************info_ok  "+info_ok);
+        log.info("*******************info_ok  " + info_ok);
         return info_ok;
     }
 
+    /**
+     * 超时操作
+     * @param id
+     * @return
+     */
     @GetMapping("/consumer/payment/hystrix/timeOut/{id}")
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+    })
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         String info_timeOut = paymentHystrixService.paymentInfo_TimeOut(id);
-        log.info("********************info_timeOut  "+info_timeOut);
+        log.info("********************info_timeOut  " + info_timeOut);
         return info_timeOut;
+    }
+    /**
+     * 备用方法
+     * @param id
+     * @return
+     */
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "我是消费者80，对方系统繁忙请稍后再试！";
+
     }
 }
