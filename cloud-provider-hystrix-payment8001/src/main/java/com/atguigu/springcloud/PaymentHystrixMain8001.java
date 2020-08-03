c5081dede5eb85a1412c9baf418d291a07ef2c94
xuhao <1017504059@qq.com>
2020-08-03 14:24:49 +0800

    8001配置进Dashboard

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
@@ -27,5 +27,14 @@ public class PaymentHystrixMain8001
      *ServletRegistrationBean因为springboot的默认路径不是"/hystrix.stream"，
      *只要在自己的项目里配置上下面的servlet就可以了
      */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 
 }
