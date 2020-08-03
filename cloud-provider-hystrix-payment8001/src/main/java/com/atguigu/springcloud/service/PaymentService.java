3817b8a69c99df25e3f8a7fb9256107af4f2c766
xuhao <1017504059@qq.com>
2020-08-03 09:36:47 +0800

    8001对自己进行的降级保护

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.atguigu.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -22,14 +24,28 @@ public class PaymentService {
      * @param id
      * @return
      */
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="5000")
+    })
     public String paymentInfo_TimeOut(Integer id) {
-        //int age = 10/0;
-        int timeNum = 5000;
+//        int age = 10/0;
+        int timeNum = 3000;
         try {
             TimeUnit.MILLISECONDS.sleep(timeNum);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return "线程池:  " + Thread.currentThread().getName() + " id:  " + id + "\t" + "耗时:" + timeNum;
+       return "线程池:  " + Thread.currentThread().getName() + " id:  " + id + "\t" + "耗时:" + timeNum;
+//        return "线程池:  " + Thread.currentThread().getName() + " id:  " + id + "\t";
+    }
+
+    /**
+     * 备用方法
+     * @param id
+     * @return
+     */
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "线程池:  " + Thread.currentThread().getName() + " id:  " + id + "\t" + "8001系统繁忙或者运行出错，请稍后";
+
     }
 }
