988684524fef074f29a90c27395c43c455265313
xuhao <1017504059@qq.com>
2020-08-07 13:38:13 +0800

    config3366搭建

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @auther xuhao
+ * @create 2020年8月7日13:37:09
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
