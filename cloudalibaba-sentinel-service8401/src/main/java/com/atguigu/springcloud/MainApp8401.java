e7bc721192358842450a88e257ef5996012ab94f
xuhao <1017504059@qq.com>
2020-08-17 16:11:58 +0800

    Sentinel 8401 模块搭建

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/MainApp8401.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @auther xuhao
+ * @create 2020年8月17日16:04:58
+ *
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MainApp8401
+{
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 描述：
+ *
+ * @author XuHao
+ * @date 2020/8/17  16:05
+ **/
+@RestController
+public class FlowLimitController {
+}
