b83caa011c204f2f60181bf4d1809703a1f477eb
xuhao <1017504059@qq.com>
2020-08-17 16:21:20 +0800

    Sentinel 8401 模块搭建

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -1,5 +1,6 @@
 package com.atguigu.springcloud.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,4 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 public class FlowLimitController {
+    @GetMapping("/testA")
+    public String testA(){
+        return "-----testA";
+    }
+    @GetMapping("/testB")
+    public String testB(){
+        return "-----testB";
+    }
 }
