25db9209113a54ea87e7866dc06e34dacb5b7fdd
xuhao <1017504059@qq.com>
2020-08-18 15:20:36 +0800

    笔记更新

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -1,8 +1,11 @@
 package com.atguigu.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.sun.deploy.security.BlockedException;
 import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -34,10 +37,26 @@ public class FlowLimitController {
 
     @GetMapping("/testD")
     public String testD() {
-        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
-        log.info("testD 测试RT");
-//        log.info("testD 异常比例");
-//        int age = 10 / 0;
+//        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+//        log.info("testD 测试RT");
+        log.info("testD 异常比例");
+        int age = 10 / 0;
         return "------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE() {
+        log.info("testE  测试异常数");
+        int age = 10 / 0;
+        return "------testE";
+    }
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false)String p1,
+                             @RequestParam(value = "p2",required = false)String p2){
+        return "------testHotKey";
+    }
+    public String deal_testHotKey(String p1, String p2, BlockedException exception){
+        return "------deal_testHotKey";
     }
 }
