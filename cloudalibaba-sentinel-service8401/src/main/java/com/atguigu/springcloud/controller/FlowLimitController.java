2aff615ecdeda7a766aacbddd985f0544f0303ef
xuhao <1017504059@qq.com>
2020-08-18 13:11:07 +0800

    Sentinel 8401 模块搭建

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -25,9 +25,19 @@ public class FlowLimitController {
 //        }
         return "-----testA";
     }
+
     @GetMapping("/testB")
-    public String testB(){
-        log.info(Thread.currentThread().getName()+"\t"+"....testB");
+    public String testB() {
+        log.info(Thread.currentThread().getName() + "\t" + "....testB");
         return "-----testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+        log.info("testD 测试RT");
+//        log.info("testD 异常比例");
+//        int age = 10 / 0;
+        return "------testD";
     }
 }
