a013fc7397283245b8b93702f7d94dc1adcbbf3f
xuhao <1017504059@qq.com>
2020-08-19 14:54:53 +0800

    HotKey配置

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/RateLmitController.java
@@ -0,0 +1,35 @@
+package com.atguigu.springcloud.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.atguigu.springcloud.entity.CommonResult;
+import com.atguigu.springcloud.entity.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 描述：
+ *
+ * @author XuHao
+ * @date 2020/8/19  13:32
+ **/
+@RestController
+@Slf4j
+public class RateLmitController {
+    @GetMapping("/byResource")
+    @SentinelResource(value = "byResource",blockHandler = "handleException")
+    public CommonResult byResource()
+    {
+        return new CommonResult(200,"按资源名称限流测试OK",new Payment(2020L,"serial001"));
+    }
+    public CommonResult handleException(BlockException exception)
+    {
+        return new CommonResult(444,exception.getClass().getCanonicalName()+"\t 服务不可用");
+    }
+    @GetMapping("/rateLimit/byUrl")
+    @SentinelResource(value = "byUrl")
+    public CommonResult byUrl(){
+        return new CommonResult(200,"按url限流测试ok",new Payment(2020L,"serial002"));
+    }
+}
