0170cbe7519f57ba0f610cdc3940e642935da865
xuhao <1017504059@qq.com>
2020-08-20 17:53:48 +0800

    84搭建

--- cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/controller/CircleBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/atguigu/springcloud/controller/CircleBreakerController.java
@@ -26,11 +26,8 @@ public class CircleBreakerController {
     @Resource
     private RestTemplate restTemplate;
     @RequestMapping("/consumer/fallback/{id}")
-    @SentinelResource(value = "fallback") //没有配置
-    //@SentinelResource(value = "fallback",fallback = "handlerFallback") //fallback只负责业务异常
-    //@SentinelResource(value = "fallback",blockHandler = "blockHandler") //blockHandler只负责sentinel控制台配置违规
-   //@SentinelResource(value = "fallback",fallback = "handlerFallback",blockHandler = "blockHandler",
-    //        exceptionsToIgnore = {IllegalArgumentException.class})
+   // @SentinelResource(value = "fallback") //没有配置
+    @SentinelResource(value = "fallback",fallback = "handlerFallback") //没有配置
     public CommonResult<Payment> fallback(@PathVariable Long id)
     {
         CommonResult<Payment> result = restTemplate.getForObject(SERVICE_URL + "/paymentSQL/"+id,CommonResult.class,id);
@@ -42,6 +39,11 @@ public class CircleBreakerController {
         }
 
         return result;
+    }
+    //本例是fallback
+    public CommonResult handlerFallback(@PathVariable  Long id,Throwable e) {
+        Payment payment = new Payment(id,"null");
+        return new CommonResult<>(444,"兜底异常handlerFallback,exception内容  "+e.getMessage(),payment);
     }
 
 }
