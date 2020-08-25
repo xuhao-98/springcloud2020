cd56760950fb86cec9a76bb7a12e3a841b72e975
xuhao <1017504059@qq.com>
2020-08-25 14:00:47 +0800

    seata数据库表

---
+++ t_order.sql
@@ -0,0 +1,34 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : xxx
+ Source Server Type    : MySQL
+ Source Server Version : 50715
+ Source Host           : localhost:3306
+ Source Schema         : seata_order
+
+ Target Server Type    : MySQL
+ Target Server Version : 50715
+ File Encoding         : 65001
+
+ Date: 25/08/2020 14:00:04
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_order
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order`;
+CREATE TABLE `t_order`  (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(11) DEFAULT NULL COMMENT '用户id',
+  `product_id` bigint(11) DEFAULT NULL COMMENT '产品id',
+  `count` int(11) DEFAULT NULL COMMENT '数量',
+  `money` decimal(11, 0) DEFAULT NULL COMMENT '金额',
+  `status` int(1) DEFAULT NULL COMMENT '订单状态：0：创建中；1：已完结',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
