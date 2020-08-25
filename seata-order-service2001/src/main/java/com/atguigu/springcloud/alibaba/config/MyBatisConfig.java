6e4bfea801ea070a8860eca1aef0c30c0fb1ebdb
xuhao <1017504059@qq.com>
2020-08-25 16:13:23 +0800

    订单2001 构建

--- seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/config/MyBatisConfig.java
+++ seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/config/MyBatisConfig.java
@@ -1,10 +1,15 @@
 package com.atguigu.springcloud.alibaba.config;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * 描述：
  *
  * @author XuHao
  * @date 2020/8/25  14:29
  **/
+@Configuration
+@MapperScan({"com.atguigu.springcloud.alibaba.dao"})
 public class MyBatisConfig {
 }
--- seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/controller/OrderController.java
+++ seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/controller/OrderController.java
@@ -1,10 +1,30 @@
 package com.atguigu.springcloud.alibaba.controller;
 
+import com.atguigu.springcloud.alibaba.domain.CommonResult;
+import com.atguigu.springcloud.alibaba.domain.Order;
+import com.atguigu.springcloud.alibaba.service.OrderService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
 /**
  * 描述：
  *
  * @author XuHao
  * @date 2020/8/25  14:30
  **/
-public class OrderController {
+@RestController
+public class OrderController
+{
+    @Resource
+    private OrderService orderService;
+
+
+    @GetMapping("/order/create")
+    public CommonResult create(Order order)
+    {
+        orderService.create(order);
+        return new CommonResult(200,"订单创建成功");
+    }
 }
